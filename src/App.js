b5bb2ddaa70671a0bb36d5c26aeccdf569455809
Nikhil <nikhildgr788@gmail.com>
2024-05-05 03:31:46 +0530

    Assigment Done.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [userData, setUserData] = useState("");
+  const getData = async() => {
+    try {
+      const url = `https://randomuser.me/api/?page=1&results=1&seed=abc`
+      let res = await fetch(url);
+      let data = await res.json();
+
+      const {first, last} = data.results[0].name;
+      const {gender} = data.results[0];
+      const {phone} = data.results[0];
+      const {large} = data.results[0].picture;
+
+      // console.log(large);
+
+      const myData = {
+        first,last,gender,phone,large,
+      };
+
+      setUserData(myData);
+
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    getData();
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="card">
+        <div className="lines"></div>
+        <div className="imgBx">
+          <img src= {userData.large} alt="User Profile" />
+        </div>
+        <div className="content">
+          <div className="details">
+            <h2>{`${userData.first} ${userData.last}`}</h2>
+            <div className="data">
+              <h3> <span>Gender</span>{`${userData.gender}`}</h3>
+              <h3> <span>phone Number </span>{`${userData.phone}` }</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
